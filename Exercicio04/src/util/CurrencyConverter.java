bbccd153ac4301f8bc5b469c3a4541cb87630cd5
DanielFerreiraDosSantosRosas <danielferreiradossantosrosas@gmail.com>
2023-01-06 19:34:15 -0300

    Exercicio 04 contendo menbros estaticos

---
+++ Exercicio04/src/util/CurrencyConverter.java
@@ -0,0 +1,13 @@
+package util;
+
+public class CurrencyConverter {
+	
+	public static double IOF = 1.06;
+	
+	public static double dollarToReal(double amount, double dollarPrice) {
+		
+	return amount * dollarPrice *  IOF;
+	
+	}
+
+}
